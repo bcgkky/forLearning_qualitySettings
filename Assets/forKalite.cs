5a7c3a9de1a8431b75f7faed69559472338ac0bf
bcgkky <bcgkky1@gmail.com>
2023-12-07 13:30:56 +0300

    last
    ...

---
+++ Assets/forKalite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class forKalite : MonoBehaviour
+{
+
+    public Dropdown kaliteDropDown;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Kalite"))
+        {
+            kaliteDropDown.value = PlayerPrefs.GetInt("Kalite");
+            QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("Kalite"));
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Kalite", 2);
+            QualitySettings.SetQualityLevel(2);
+            kaliteDropDown.value = 2;
+        }
+    }
+    public void forKaliteAyar(int kalite)
+    {
+        PlayerPrefs.SetInt("Kalite" , kalite);
+
+        QualitySettings.SetQualityLevel(kalite);
+    }
+}
